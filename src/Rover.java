5cac7fa27d1a97a29dc11a2992c62268d1f27047
Cominc <cominc@outlook.it>
2018-01-23 00:34:33 +0100

    Draft implementation of the Rover class

--- src/Rover.java
+++ src/Rover.java
@@ -1,5 +1,38 @@
+import java.awt.Point;
 
 
 public class Rover {
+	private String direction;
+	private Point position;
+	
+	public Rover(String direction, Point position) {
+		super();
+		this.direction = direction;
+		this.position = position;
+	}
+
+	public String getDirection() {
+		return direction;
+	}
+
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
+
+	public Point getPosition() {
+		return position;
+	}
+
+	public void setPosition(Point position) {
+		this.position = position;
+	}
+	
+	public void turnLeft(){
+		//TODO implementare metodo
+	}
+	
+	public void turnRight(){
+		//TODO implementare metodo
+	}
 	
 }
